17d977f0d2abd2b3dc941dd7948c98ef7427a89d
Dilshad khan <97806509+Dilshad0404@users.noreply.github.com>
2022-03-15 22:51:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var button= document.querySelector("#btn");
+  var input = document.querySelector("#input-area");
+  var output = document.querySelector("#output")
+
+  button.addEventListener('click', function converter(){
+
+    output.innerHTML=input.value*77.38 + " INR "
+
+  })
+
+
+  var button2=document.querySelector("#btn2");
+  var input2=document.querySelector("#input-area2");
+  var output2=document.querySelector("#output2");
+
+  button2.addEventListener('click', function converter(){
+
+    output2.innerHTML=input2.value*99.91 + " INR "
+
+  })
+
+
+  var button3=document.querySelector("#btn3");
+  var input3=document.querySelector("#input-area3");
+  var output3=document.querySelector("#output3");
+
+  button3.addEventListener('click', function converter(){
+
+    output3.innerHTML=input3.value*20.36+ " INR "
+
+  })
+
+
+
+  var button4=document.querySelector("#btn4");
+  var input4=document.querySelector("#input-area4");
+  var output4=document.querySelector("#output4");
+
+  button4.addEventListener('click', function converter(){
+
+    output4.innerHTML=input4.value*2.34+ " PKR "
+
+  })
